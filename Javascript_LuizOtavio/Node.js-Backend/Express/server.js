ca7675b3dd0d6eb7d3aa6ca15d275cccb8d949ed
Julia-Marcal <anajulia.05marcal@gmail.com>
2022-12-07 15:34:50 -0300

    feat: express querys,params e mostrando resposta do form no site

--- Javascript_LuizOtavio/Node.js-Backend/Express/server.js
+++ Javascript_LuizOtavio/Node.js-Backend/Express/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(
+    express.urlencoded(
+        { extended : true}
+        )
+    )
+
 //CRUD -> CREATE, READ, UPDATE, DELETE
 //        POST    GET   PUT     DELETE
 
@@ -9,18 +15,27 @@ const app = express();
 app.get('/', (req,res)=> {
     res.send(`
     <form action="/" method="POST">
-    Nome: <input type="text" name="nome"></input>
+    Nome: <input type="text" name="nome"><br>
+    Idae: <input type="number" name="idade">
     <button>Enviar</button>
     </form>
     `);
 });
 
-app.post('/', (req, res) =>{
-    res.send('Recebi o formulário')
+//a interrogação torna o parametro opcional
+//req.params acessa os parametros da url
+//req.query acessa oq estiver na query
+
+// /profiles/?chave1=valor1&chave2=valor2
+// paramentro/ query    separação query     
+
+
+app.get('/test/:idUsuarios?', (req, res)=>{
+    res.send('oi')
 })
 
-app.get('/contato', (req, res) =>{
-    res.send('Obrigado por entrar em contato com a gente')
+app.post('/', (req, res) =>{
+    res.send(`Olá ${req.body.nome} Idade:${req.body.idade}`)
 })
 
 app.listen(3000, () =>{
