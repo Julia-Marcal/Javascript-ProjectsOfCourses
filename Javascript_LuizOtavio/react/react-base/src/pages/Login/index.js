2bbbd1fbc53b1b8fd8e8a73f1e7f251d2a0b5db6
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-02-15 10:54:16 -0300

    feat: react att and api att

---
+++ Javascript_LuizOtavio/react/react-base/src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Tittle, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Tittle>
+        Login
+        <small>Oie</small>
+      </Tittle>
+      <Paragrafo>Lorem ipsum dolor sit amet</Paragrafo>
+    </Container>
+  );
+}
---
+++ Javascript_LuizOtavio/react/react-base/src/pages/Login/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Tittle = styled.h1`
+  background: red;
+  small {
+    font-size: 12pt;
+  }
+`;
+
+export const Paragrafo = styled.p``;
