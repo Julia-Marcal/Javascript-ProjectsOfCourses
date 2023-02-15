0d5562ab0c9cccfd33d1b2ec7ce3c0ba76198425
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-02-15 16:33:26 -0300

    feat: junção da api com react

--- Javascript_LuizOtavio/Consumo _de_api_com_react/src/pages/Login copy 2/Login/index.js
+++ Javascript_LuizOtavio/Consumo _de_api_com_react/src/pages/Login copy 2/Login/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container } from '../../styles/GlobalStyles';
+import { Container } from '../../../styles/GlobalStyles';
 
 export default function Login() {
   return (
